ba097bfad1ff84bf18bc8e263921fcf81357a0fd
evanbillet <evanfreebusiness@gmail.com>
2023-05-13 15:26:50 +0200

    fix merge conflict2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Snapshot from "./Usesnapshot"
 import Glbgroup from "./glbgroup"
 import Test from "./test"
 import Viewer from "./robot"
+<<<<<<< HEAD
+=======
+import Cannon from "./simple physics demo with debug bounds"
+import Shadermaterial from "./Shadermaterials"
+
+
+
+
+
+
+>>>>>>> 40f74f8 (useful shaderMaterial)
 
 export default function App() {
   return (
     <>
+<<<<<<< HEAD
       <Test />
       <Viewer />
+=======
+      <Shadermaterial />
+>>>>>>> 40f74f8 (useful shaderMaterial)
     </>
 =======
 export default function App() {
